5a4743aa08e9ddf88e6fdb27baba47428a8988e6
nexrom <m.rohrmueller@gmail.com>
2019-12-02 21:31:26 +0100

    started Archive Interface

---
+++ Common/IArchive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace Common
+{
+    public interface IArchive
+    {
+        //creates a new zip archive
+        void create();
+
+        //opens an existing zip archive
+        void open(ZipArchiveMode mode);
+
+
+
+
+    }
+}
