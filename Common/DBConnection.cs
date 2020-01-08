4a7251bfd5e1cd39621bd6e695851734c4b84c86
nexrom <m.rohrmueller@gmail.com>
2020-01-08 21:52:50 +0100

    #impl: started SQL Server integration

---
+++ Common/DBConnection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class DBConnection
+    {
+        private string server, database, user, password;
+
+        public DBConnection(string server, string database, string user, string password)
+        {
+            this.server = server;
+            this.database = database;
+            this.user = user;
+            this.password = password;
+        }
+
+        //sends a sql query
+        public List<Dictionary<string, string>> singleQuery(string query, Dictionary<string, string> parameters)
+        {
+            //build connection string
+            string connectionString = $"Server={this.server};Database={this.database};User Id={this.user};Password={this.password};";
+
+
+            //start sql connection
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                //add all query parameters
+                if (parameters != null)
+                {
+                    foreach (string key in parameters.Keys)
+                    {
+                        command.Parameters.AddWithValue(key, parameters[key]);
+                    }
+                }
+                
+                //open DB connection
+                connection.Open();
+                
+                
+                SqlDataReader reader = command.ExecuteReader();
+
+                //retVal is a list of dictionaries
+                List<Dictionary<string, string>> result = new List<Dictionary<string, string>>();
+
+                try
+                {
+                    //iterate through all results
+                    while (reader.HasRows && reader.Read())
+                    {
+                        Dictionary<string, string> resultRow = new Dictionary<string, string>();
+                        //read all columns
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            string columnName = reader.GetName(i);
+                            resultRow.Add(columnName, reader[columnName].ToString().Trim()); ;
+                        }
+
+                        //add dictionary to result list
+                        result.Add(resultRow);
+
+                    }
+                }
+                finally
+                {
+                    // Always call close when done reading
+                    reader.Close();
+                    connection.Close();
+                }
+                return result;
+            }
+
+        }
+    }
+}
--- TestProj/Program.cs
+++ TestProj/Program.cs
@@ -10,41 +10,12 @@ namespace TestProj
     {
         static void Main(string[] args)
         {
-            Common.IArchive arch = new Common.LZ4Archive("E:\\nxm\\Win10\\ef065c55-2642-41ef-bb7f-7b6c8f1dd323.nxm", null);
-            arch.getFile("Win10.vhdx.cb", "c:\\restore\\test.cb");
+            Common.DBConnection conn = new Common.DBConnection(".\\SQLEXPRESS", "nxmBackup", "nxm", "test123");
 
-            //System.IO.Stream diffStream = arch.openAndGetFileStream("Win10.vhdx.cb");
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("@id", "2");
 
-            ////read block count
-            //byte[] buffer = new byte[4];
-            //diffStream.Read(buffer, 0, 4);
-            //UInt32 blockCount = BitConverter.ToUInt32(buffer, 0);
-
-            ////restored bytes count for progress calculation
-            //long bytesRestored = 0;
-            //string lastProgress = "";
-
-            ////iterate through all blocks
-            //for (int i = 0; i < blockCount; i++)
-            //{
-            //    ulong offset;
-            //    ulong length;
-
-            //    //read block offset
-            //    buffer = new byte[8];
-            //    diffStream.Read(buffer, 0, 8);
-            //    offset = BitConverter.ToUInt64(buffer, 0);
-
-            //    //read block length
-            //    int b = diffStream.Read(buffer, 0, 8);
-            //    length = BitConverter.ToUInt64(buffer, 0);
-
-            //    //read data block
-            //    buffer = new byte[length];
-            //    int a = diffStream.Read(buffer, 0, buffer.Length);
-
-            //}
-            //diffStream.Close();
+            List<Dictionary<string, string>> retVal = conn.singleQuery("SELECT * FROM Compression WHERE id= @id", parameters);
         }
     }
 }
