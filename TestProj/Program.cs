8a7acd09de19a35b0d848e37d7b2c66a2ac118d0
nexrom <m.rohrmueller@gmail.com>
2020-01-10 22:45:06 +0100

    #impl: added optional transactions to DB Handler

--- TestProj/Program.cs
+++ TestProj/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace TestProj
 {
@@ -12,10 +13,20 @@ namespace TestProj
         {
             Common.DBConnection conn = new Common.DBConnection(".\\SQLEXPRESS", "nxmBackup", "nxm", "test123");
 
-            Dictionary<string, string> parameters = new Dictionary<string, string>();
-            parameters.Add("@id", "2");
+            SqlTransaction transaction = conn.beginTransaction();
 
-            List<Dictionary<string, string>> retVal = conn.singleQuery("SELECT * FROM Compression WHERE id= @id", parameters);
+            for (int i = 0; i < 10; i++)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                parameters.Add("@name", "TestJob" + i.ToString());
+
+                List<Dictionary<string, string>> retVal = conn.doQuery("INSERT INTO compression (name) VALUES (@name)", parameters, transaction);
+            }
+
+            transaction.Rollback();
+
+            conn.Dispose();
+
         }
     }
 }
