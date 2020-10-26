c5a4c440b1c210487939e7a4874245241ffa96c0
Matthias Rohrmüller <m.rohrmueller@gmail.com>
2020-10-26 21:30:49 +0100

    #impl: started liverestore

---
+++ nxmBackup/RestoreHelper/LiveRestore.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoreHelper
+{
+    public class LiveRestore
+    {
+        public void performLiveRestore(string basePath, string destPath, string vmName, string instanceID, bool importToHyperV)
+        {
+         
+
+            //get full backup chain
+            List<ConfigHandler.BackupConfigHandler.BackupInfo> backupChain = ConfigHandler.BackupConfigHandler.readChain(basePath);
+
+            //look for the desired instanceid
+            ConfigHandler.BackupConfigHandler.BackupInfo targetBackup = getBackup(backupChain, instanceID);
+
+            //target backup found?
+            if (targetBackup.instanceID != instanceID )
+            {
+
+                return; //not found, no restore
+            }
+
+            //build restore chain, top down (full backup is last element)
+            List<ConfigHandler.BackupConfigHandler.BackupInfo> restoreChain = new List<ConfigHandler.BackupConfigHandler.BackupInfo>();
+
+            //add target chain element first
+            restoreChain.Add(targetBackup);
+
+            //look for backup element until full backup found
+            while (restoreChain[restoreChain.Count - 1].type != "full")
+            {
+                ConfigHandler.BackupConfigHandler.BackupInfo restoreElement = getBackup(backupChain, restoreChain[restoreChain.Count - 1].parentInstanceID);
+
+                //just first backup can be "lb", ignore it here
+                if (restoreElement.type == "lb")
+                {
+                    continue;
+                }
+
+                //valid element found?
+                if (restoreElement.instanceID != restoreChain[restoreChain.Count - 1].parentInstanceID)
+                {
+                    //element is not valid, chain is broken, cancel restore
+                    return;
+                }
+                else
+                {
+                    //valid element found
+                    restoreChain.Add(restoreElement);
+                }
+            }
+
+            
+        }
+
+        private ConfigHandler.BackupConfigHandler.BackupInfo getBackup(List<ConfigHandler.BackupConfigHandler.BackupInfo> backupChain, string instanceID)
+        {
+            foreach (ConfigHandler.BackupConfigHandler.BackupInfo backup in backupChain)
+            {
+                if (backup.instanceID == instanceID)
+                {
+                    return backup;
+                }
+            }
+
+            //backup not found, return empty backup element
+            return new ConfigHandler.BackupConfigHandler.BackupInfo();
+        }
+    }
+}
