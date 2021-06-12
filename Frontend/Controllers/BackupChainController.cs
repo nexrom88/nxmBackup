019df0e613f6eb8a0d70346ebbe376ba75d3d537
Matthias Rohrmüller <m.rohrmueller@gmail.com>
2021-06-12 17:26:55 +0200

    #impl: some workings on restore options GUI

---
+++ Frontend/Controllers/BackupChainController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Frontend.Controllers
+{
+    public class BackupChainController : ApiController
+    {
+        // POST api/<controller>
+        public void Post([FromBody] RestoreDetails jobDetails)
+        {
+            string sourcePath = jobDetails.basePath + "\\" + jobDetails.jobName + "\\" + jobDetails.vmName;
+
+            //read backup chain
+            List<ConfigHandler.BackupConfigHandler.BackupInfo> backups = ConfigHandler.BackupConfigHandler.readChain(sourcePath);
+
+            backups = null;
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+
+        public class RestoreDetails
+        {
+            public string basePath { get; set; }
+            public string jobName { get; set; }
+            public string vmName { get; set; }
+        }
+    }
+}
--- Frontend/Scripts/nxmRestore.js
+++ Frontend/Scripts/nxmRestore.js
@@ -1,7 +1,18 @@
-﻿
+﻿var selectedRestoreJob = {}; //the job selected for restore
+
 //starts the restore process
 function startRestoreHandler() {
   var restoreJobID = selectedJob;
+
+  var jobObj = {};
+  //look for the job object
+  for (var i = 0; i < configuredJobs.length; i++) {
+    if (configuredJobs[i].DbId == restoreJobID) {
+      jobObj = configuredJobs[i];
+      selectedRestoreJob = jobObj;
+      break;
+    }
+  }
 
   //show overlay
   $("#restoreOverlay").css("display", "block");
@@ -10,7 +21,33 @@ function startRestoreHandler() {
   $.ajax({
     url: "Templates/restoreOptions"
   }).done(function (data) {
-    $("#restoreOptions").html(data);
+
+    //load job vms
+    var vms = [];
+    for (var i = 0; i < jobObj.JobVMs.length; i++) {
+      vms[i] = { vmid: jobObj.JobVMs[i].vmID, name: jobObj.JobVMs[i].vmName };
+    }
+
+    var vmsHTML = Mustache.render(data, { vms: vms });
+
+    $("#restoreOptions").html(vmsHTML);
+
+    loadRestorePoints();
   });
+
+}
+
+//load restore points
+function loadRestorePoints() {
+
+  //build object to query restore points
+  var restoreDetails = {};
+  restoreDetails["jobName"] = selectedRestoreJob["Name"];
+  restoreDetails["basePath"] = selectedRestoreJob["BasePath"];
+
+  //get selected vm
+  var selectedVM = $("#sbSourceVM option:selected").text();
+  restoreDetails["vmName"] = selectedVM;
+
 
 }
