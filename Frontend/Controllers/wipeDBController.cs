502a4b198b773b8c624d89257adc287eb3469a7a
nexrom <mr@nexrom.de>
2022-04-10 14:21:44 +0200

    #impl: reset from GUI now possible

---
+++ Frontend/Controllers/wipeDBController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Frontend.Controllers
+{
+    public class wipeDBController : ApiController
+    {
+        // GET api/<controller>/5
+        public void Get()
+        {
+            Common.DBQueries.wipeDB();
+        }
+
+       
+    }
+}
