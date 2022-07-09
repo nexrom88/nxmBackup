b53a5356fa96c7e412d8fc7a62f4987d9d8f6315
nexrom <mr@nexrom.de>
2022-07-09 23:57:46 +0200

    #temporary commit

--- Frontend/Scripts/nxmHelp.js
+++ Frontend/Scripts/nxmHelp.js
@@ -1,1 +1,24 @@
-﻿
+﻿//shows the help form
+function showHelpForm() {
+    //show dialog box
+    Swal.fire({
+        title: 'Hilfe',
+        html: "<div id='helpForm'></div>",
+        confirmButtonColor: '#3085d6',
+        allowOutsideClick: true,
+        allowEscapeKey: true,
+        confirmButtonText: 'ok'
+
+    });
+
+    //load settings form
+    $.ajax({
+        url: "Templates/helpForm"
+    })
+        .done(function (settingsForm) {
+            $("#settingsPopUp").html(settingsForm);
+
+            //show settings
+            $("#inputMountPath").val(globalSettings["mountpath"]);
+        });
+}
