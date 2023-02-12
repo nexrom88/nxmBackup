fef1b41c479efbfe41afa8ec2370db54ba0bc84f
nexrom <mr@nexrom.de>
2023-02-12 13:17:32 +0100

    first workings

---
+++ CoreService/Service1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.IO.File.WriteAllText(@"h:\test.txt", "test");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
