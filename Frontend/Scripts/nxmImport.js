9759746243fa24a993433fd4cd7272219a57f13a
Matthias Rohrmüller <mr@nexrom.de>
2024-06-01 20:36:33 +0200

    some workings for importing backups

--- Frontend/Scripts/nxmImport.js
+++ Frontend/Scripts/nxmImport.js
@@ -4,6 +4,15 @@ function startImportBackupProcess() {
     $.ajax({
         url: "Templates/selectImportSource"
     }).done(function (data) {
-        
+        data = replaceLanguageMarkups(data);
+
+        Swal.fire({
+            title: languageStrings["import_backup"],
+            html: data,
+            confirmButtonColor: '#3085d6',
+            allowOutsideClick: true,
+            allowEscapeKey: true,
+            confirmButtonText: languageStrings["close"],
+        });
     });
 }
