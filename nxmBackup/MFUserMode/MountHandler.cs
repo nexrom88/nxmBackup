09684d22a2de828970e9a08ab3fe6f6860a687c1
nexrom <mr@nexrom.de>
2024-09-20 11:55:25 +0200

    fixed bug when lr could not be startet due to case-sensitive mount path

--- nxmBackup/MFUserMode/MountHandler.cs
+++ nxmBackup/MFUserMode/MountHandler.cs
@@ -17,6 +17,13 @@ namespace nxmBackup.MFUserMode
         [DllImport("Kernel32.dll", CharSet = CharSet.Unicode)]
         private static extern uint QueryDosDevice([In] string lpDeviceName, [Out] StringBuilder lpTargetPath, [In] int ucchMax);
 
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint GetLongPathName(string ShortPath, StringBuilder sb, int buffer);
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetShortPathName(string longpath, StringBuilder sb, int buffer);
+
+
         private MFUserMode kmConnection;
         private bool processStopped = false;
         private System.IO.FileStream destStream;
@@ -462,6 +469,9 @@ namespace nxmBackup.MFUserMode
                 return null;
             }
 
+            //get physical path from path-obj to prevent case-sensitive errors
+            mountPath = getWindowsPhysicalPath(mountPath);
+
             DriveInfo drive = new DriveInfo((Path.GetPathRoot(mountPath)));
 
             try
@@ -648,6 +658,39 @@ namespace nxmBackup.MFUserMode
                 }
             }
 
+        }
+
+        //returns the case-sensitive path when given a case-insensitive path
+        private string getWindowsPhysicalPath(string path)
+        {
+            StringBuilder builder = new StringBuilder(255);
+
+            // names with long extension can cause the short name to be actually larger than
+            // the long name.
+            GetShortPathName(path, builder, builder.Capacity);
+
+            path = builder.ToString();
+
+            uint result = GetLongPathName(path, builder, builder.Capacity);
+
+            if (result > 0 && result < builder.Capacity)
+            {
+                //Success retrieved long file name
+                builder[0] = char.ToLower(builder[0]);
+                return builder.ToString(0, (int)result);
+            }
+
+            if (result > 0)
+            {
+                //Need more capacity in the buffer
+                //specified in the result variable
+                builder = new StringBuilder((int)result);
+                result = GetLongPathName(path, builder, builder.Capacity);
+                builder[0] = char.ToLower(builder[0]);
+                return builder.ToString(0, (int)result);
+            }
+
+            return null;
         }
 
         public enum ProcessState
